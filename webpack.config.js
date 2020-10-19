4404622c420299c04a55bf4a932d9987bf3b21f8
Roman-Statsura <srs1012@yandex.ru>
2020-10-19 23:16:01 +0400

    fixed problem with img

--- webpack.config.js
+++ webpack.config.js
@@ -41,7 +41,7 @@ module.exports = {
         ],
       },
       {
-        test: /\.(gif|png|jpe?g|svg)$/i,
+        test: /\.(png|jpg|gif|ico|svg)$/i,
         use: [
           {
             loader: 'file-loader',
@@ -51,24 +51,8 @@ module.exports = {
             },
           },
           {
-            loader: "image-webpack-loader",
+            loader: 'image-webpack-loader',
             options: {
-              mozjpeg: {
-                progressive: true,
-              },
-              optipng: {
-                enabled: false,
-              },
-              pngquant: {
-                quality: [0.65, 0.9],
-                speed: 4,
-              },
-              gifsicle: {
-                interlaced: false,
-              },
-              webp: {
-                quality: 75,
-              },
             },
           },
         ],
